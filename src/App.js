601d18c0658260669a593983aea5ebb2e54d0cbf
Guy <guymoustapha@gmail.com>
2021-04-14 21:57:48 +0200

    setup linters

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>welcome to tthe App built from east africa</h1>
-      <p>Welcome to our app</p>
+        <h1>welcome to tthe App built from east africa</h1>
+        <p>Welcome to our app</p>
+        <h3>Guy & Roy </h3>
       </header>
     </div>
   );
