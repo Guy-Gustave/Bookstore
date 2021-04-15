ea169c1d7d0738fd8b416c71d809985a06731067
RNtaate <royhyde14@gmail.com>
2021-04-15 14:02:17 +0300

    Create, implement book component draft.

---
+++ src/components/Book.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+let Book = (props) => {
+  const {title, category} = props;
+  return(
+    <>
+      <h2>Title: {title}</h2>
+      <p>Category: {category}</p>
+    </>
+  )
+}
+
+Book.defaultProps = {
+  title: '',
+  category: ''
+}
+
+Book.propTypes = {
+  title: PropTypes.string,
+  category: PropTypes.string
+}
+
+export default Book;
