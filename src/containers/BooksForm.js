d7bdf98196fdd86627bbc02ac4786306b6c8364e
RNtaate <royhyde14@gmail.com>
2021-04-15 13:23:08 +0300

    Create BooksList, BooksForm components in containers folder

---
+++ src/containers/BooksForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+let BooksForm = () => {
+  return (
+    <h2>This is a books form</h2>
+  )
+}
+
+export default BooksForm;
---
+++ src/containers/BooksList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+let BooksList = () => {
+  return(
+    <h1>This is a books list</h1>
+  )
+}
+
+export default BooksList;
