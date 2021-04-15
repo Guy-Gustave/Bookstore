05cb5f97230e4a77e6a77549825c7112e625d06e
RNtaate <royhyde14@gmail.com>
2021-04-15 17:41:33 +0300

    Display table data using connect

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
-let BooksList = () => {
+let BooksList = (props) => {
+  let booksArray = [...props.listBooks];
   return(
     <table>
       <tr>
@@ -8,8 +10,19 @@ let BooksList = () => {
         <th>Title</th>
         <th>Category</th>
       </tr>
+      {booksArray.map(book => <tr>
+        <td>{book.id}</td>
+        <td>{book.title}</td>
+        <td>{book.category}</td>
+      </tr>)}
     </table>
   )
 }
 
-export default BooksList;
+let mapStateToProps = (state) => {
+  return ({
+    listBooks : state
+  });
+}
+
+export default connect(mapStateToProps, null)(BooksList);
