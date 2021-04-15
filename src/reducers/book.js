5c24d31040a00f9b2383ce1d6262df36f745ca8c
RNtaate <royhyde14@gmail.com>
2021-04-15 17:40:08 +0300

    Add initial State to books reducer

--- src/reducers/book.js
+++ src/reducers/book.js
@@ -1,9 +1,33 @@
-const books = (state = [], action) => {
+let myId = () => {
+  return Math.floor(Math.random() * 100)
+};
+
+
+let initialState = [
+  {
+    id: myId(),
+    title: 'East Africa Book for Geniuses.',
+    category: 'Learning',
+  },
+  {
+    id: myId(),
+    title: 'The history of React js',
+    category: 'Biography'
+  },
+
+  {
+    id: myId(),
+    title: 'Guy lives in Burundi',
+    category: 'History'
+  }
+]
+
+const books = (state = initialState, action) => {
   switch (action.type) {
     case 'CREATE_BOOK':
       return [...state, action.book ];
     case 'REMOVE_BOOK':
-      return state.filter(book => action.id !== book.id);
+      return [ ...state.filter(book => action.id !== book.id)];
     default:
       return state;
   }
