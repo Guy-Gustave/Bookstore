b3eb8e463fba57eb73ffd7cf604a6b655fe52718
RNtaate <royhyde14@gmail.com>
2021-04-16 17:50:43 +0300

    Create handleFilter function, use props to handle select change

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const CategoryFilter = () => {
+const CategoryFilter = (props) => {
   const categories = ['All', 'Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+
+
+  const handleFilter = (e) => {
+    props.filter(e.target.value);
+  }
+
   return(
-    <select name="categorySelect">
+    <select name="categorySelect" onChange={handleFilter}>
       {categories.map(cat => <option value={cat} >{cat} </option>)}
     </select>
   )
