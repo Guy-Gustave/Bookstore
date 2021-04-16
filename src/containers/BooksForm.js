d4b37835d921065572904c718ea944f600725ac6
Guy <guymoustapha@gmail.com>
2021-04-16 12:39:47 +0200

    implement adding new book using connect

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {useState} from 'react';
+import { connect } from 'react-redux';
+import { addBook } from  '../actions/index';
 
-const BooksForm = () => {
+const BooksForm = (props) => {
 
   const [localbook, setLocalBook] = useState(
     {
@@ -25,16 +27,36 @@ const BooksForm = () => {
     }
   }
 
+  let handleSubmit = () => {
+    props.submitNewBook(localbook);
+    setLocalBook(
+      {
+        id: Math.floor(Math.random() * 100),
+        title: '',
+        category: 'Action'        
+      })
+  }
+
   const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
   return (
     <form>
-      <input type="text" placeholder="Type book title here" name='input' onChange={handleChange} />
-      <select name='select' onChange={handleChange} >
+      <input type="text" placeholder="Type book title here" name='input' onChange={handleChange} value={localbook.title}/>
+      <select name='select' onChange={handleChange} value={localbook.category} >
         {categories.map((el) => <option key={el} value={el} >{el}</option>)}
       </select>
-      <button type="button">Submit</button>
+      <button type="button" onClick={handleSubmit} >Submit</button>
     </form>
   );
 };
 
-export default BooksForm;
+let mapDispatchToProps = dispatch => {
+  return (
+    {
+      submitNewBook: (localbook) => {
+        dispatch(addBook(localbook));        
+      }
+    }
+  );
+}
+
+export default connect(null, mapDispatchToProps)(BooksForm) ;
