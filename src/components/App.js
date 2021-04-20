be941c195409e7a13fb52edbd25f5ff59195a283
Guy <guymoustapha@gmail.com>
2021-04-20 11:22:16 +0200

    import Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import '../App.css';
 import BooksList from '../containers/BooksList';
 import BooksForm from '../containers/BooksForm';
+import Navbar from './Navbar';
 
 const App = () => (
   <div className="App">
+    <Navbar />
     <BooksList />
     <BooksForm />
     <p>Project by Guy and Roy</p>
