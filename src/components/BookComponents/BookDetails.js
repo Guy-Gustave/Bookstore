a2a1ea7b486497730ada47fa9ef4307a7e82ba0f
Guy <guymoustapha@gmail.com>
2021-04-20 18:55:48 +0200

    adding author to the book

--- src/components/BookComponents/BookDetails.js
+++ src/components/BookComponents/BookDetails.js
@@ -1,21 +1,25 @@
 import React from 'react';
+import '../NavbarStyles.css';
 
 const BookDetails = ({book, remove}) => {
 
   const handleRemove = () => {
     remove(book);
   };
+  const authors = ['Roy NTAATE', 'Guy Gustave NIGABA', 'Ntaate from UGANDA', 'Ngugi wa Nthiongo', 'Adrien Ntabona'];
+
 
   return (
-    <div className="book-details-div container-fluid bg-primary" style={{width: "200%"}}>
-      <h4>{book.category}</h4>
-      <h2>{book.title}</h2>
-      <div className="book-details-buttons-div">
-        <span>Comment</span>
-        <span>
+    <div className="book-details-div container-fluid bg-light my-3" style= {{width: '200%'}} >
+      <h6 className='text-secondary'>{book.category}</h6>
+      <h4 className='title'>{book.title}</h4>
+      <span className="spin-det mb-3 text-primary font-weight-light">{authors[Math.floor(Math.random() * 5)]}</span>
+      <div className=" book-details-buttons-div d-flex">
+        <span className="pr-4 spin-det">Comment</span>
+        <span className="pr-4 spin-det ">
           <button type="button" onClick={handleRemove}>Remove Book</button>
         </span>
-        <span>Edit</span>
+        <span className="pr-4 spin-det">Edit</span>
       </div>
     </div>
   );
