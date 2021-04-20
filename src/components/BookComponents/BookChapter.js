96e71c6278853d381653d1b2f079c489e145eaa6
Guy <guymoustapha@gmail.com>
2021-04-20 18:54:52 +0200

    change style with bootstrap

--- src/components/BookComponents/BookChapter.js
+++ src/components/BookComponents/BookChapter.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const BookChapter = () => {
   return(
-    <div className="book-chapter-div container-fluid bg-info">
-      <h2>Book Chapter</h2>
+    <div className="book-chapter-div container-fluid bg-light P-3">
+      <h6 className='mt-4 text-secondary'>CURRENT CHAPTER</h6>
+      <p>Chapter 17</p>
+      <button className='btn-primary rounded px-3 mb-3' style={{fontSize: "12px"}}>UPDATE PRGRESS</button>
+
     </div>
   );
 }
--- src/components/BookComponents/BookProgress.js
+++ src/components/BookComponents/BookProgress.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 const BookProgress = () => {
-  return(
-    <div className="book-progress-div container-fluid bg-danger">
-      <h2>Book Progress</h2>
+  return (
+    <div className="progress d-flex container-fluid bg-light my-3">
+      <div className="spinner-div"></div>
+      <div className="book-progress-div px-3 mb-3">
+        <h2 className="mt-4">{Math.floor(Math.random() * 100)} %</h2>
+        <p className="h6 text-secondary">Completed</p>
+      </div>
     </div>
   );
 }
