f07d53eb40b90998d316fbfb9bbc2066f42203e7
Guy <guymoustapha@gmail.com>
2021-04-20 18:53:56 +0200

    add width

--- src/components/Book.js
+++ src/components/Book.js
@@ -9,7 +9,7 @@ const Book = ({ book, remove }) => {
     remove(book);
   };
   return (
-    <div className="book-component-div container-fluid bg-success d-flex justify-content-between align-items-center mb-3 p-0">
+    <div className="book-component-div container-fluid d-flex justify-content-between align-items-center mb-3 p-0">
       <BookDetails book={book} remove={remove}/>
       <BookProgress />
       <BookChapter />
