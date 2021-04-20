bfa3aeed2c69f6655e3e654720002a3c51b38f87
RNtaate <royhyde14@gmail.com>
2021-04-20 14:34:45 +0300

    Add stying to navbar, categoryFilter components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import CategoryFilter from './CategoryFilter';
 import { changeFilter } from '../actions/index';
 import { connect } from 'react-redux';
+import "./NavbarStyles.css"
 
 const Navbar = ({ filterCategoryName }) => {
 
@@ -10,13 +11,14 @@ const Navbar = ({ filterCategoryName }) => {
     filterCategoryName(categoryName);
   }
   return (
-  <header className="bg-white ml-4 w-100 p-3">
-    <nav className="container d-flex justify-content-arround align-items-center w-100">
+  <header className="bg-white container-fluid p-3">
+    <nav className="container d-flex justify-content-between align-items-center w-100">
       <div className="d-flex align-items-center">
         <h3 className="text-primary font-weight-bold">Bookstore CMS</h3>
-        <span className="text-secondary mx-5">Books</span>
+        <span className="text-secondary mx-5 books-span">BOOKS</span>
         <CategoryFilter filter={handleFilterChange} />
       </div>
+      <span className="text-primary d-flex justify-content-center align-items-center p-3"><i class="fas fa-user-alt"></i></span>
     </nav>
   </header>);
 };
